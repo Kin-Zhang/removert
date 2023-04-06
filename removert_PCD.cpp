d7c847ded81cf2ec75b03e9635c84a280eff9496
Kin <kinzhangglimmer@gmail.com>
2023-04-06 17:12:21 +0200

    feat: remove all the thing, setup frame for afterward refactor

---
+++ removert_PCD.cpp
@@ -0,0 +1,33 @@
+/**
+ * Copyright (C) 2022-now, RPL, KTH Royal Institute of Technology
+ * MIT License
+ * @author Kin ZHANG (https://kin-zhang.github.io/)
+ * @date: 2023-04-06 16:55
+ * @details: No ROS version, speed up the process
+ *
+ * Input: PCD files + Prior raw global map , check our benchmark in dufomap
+ * Output: Cleaned global map
+ */
+
+#include <glog/logging.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <string>
+
+int main(int argc, char** argv) {
+    google::InitGoogleLogging(argv[0]);
+    google::InstallFailureSignalHandler();
+    FLAGS_colorlogtostderr = true;
+    google::SetStderrLogging(google::INFO);
+
+    LOG(INFO) << "Start to process... octomap...";
+    // TODO
+
+
+    return 0;
+}
